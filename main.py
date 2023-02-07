d08602f32f2cce8d25358026273ff34189df4139
colerusson <colerusson2@gmail.com>
2023-02-07 16:45:50 -0700

    finished the code

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import argparse
-import numpy as np
 
 
 def main(documentsTxt):
@@ -7,19 +6,23 @@ def main(documentsTxt):
     # Make sure that your terminal output matches the terminal output of the example given on the instructions.
     wordDict = {}
     numOfLines = 0
+    # create a dictionary of all the words in the documents
     for line in documentsTxt.splitlines():
         string_list = line.split()
         numOfLines += 1
         for i in string_list:
             if i not in wordDict:
                 wordDict[i.lower()] = string_list.count(i)
-
-    array = np.empty(numOfLines, len(wordDict))
+    # put the words in alphabetical order
+    wordDict = dict(sorted(wordDict.items(), key=lambda item: item[0]))
+    # create a matrix of numOfLines x len(wordDict)
+    array = [[0 for x in range(len(wordDict))] for y in range(numOfLines)]
     # fill the matrix with the amount of times each word appears in each document
     for i in range(numOfLines):
         for j in range(len(wordDict)):
-            array[i][j] = documentsTxt[i].count(wordDict[j])
-
+            if list(wordDict.keys())[j] in documentsTxt.splitlines()[i].split():
+                array[i][j] = documentsTxt.splitlines()[i].split().count(list(wordDict.keys())[j])
+    # return the matrix
     return array
 
 
@@ -27,4 +30,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser("One Hot Encoder")
     parser.add_argument("--fpath", type=str, help="Name of the txt file to be read in")
     args = parser.parse_args()
-    main(open(args.fpath).read())
+    print('# of Features:')
+    matrix = main(open(args.fpath).read())
+    for i in range(len(matrix)):
+        print(matrix[i])
