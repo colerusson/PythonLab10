8b60ca1f8835498faa2af01e06c200b820fc555f
colerusson <colerusson2@gmail.com>
2023-02-07 16:26:57 -0700

    attempt at writing the rest of the code to fill the matrix

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import argparse
-import numpy as np
+import numpy
 
 
 def main(documentsTxt):
@@ -14,7 +14,11 @@ def main(documentsTxt):
             if i not in wordDict:
                 wordDict[i.lower()] = string_list.count(i)
 
-    array = np.empty(numOfLines, len(wordDict))
+    array = numpy.empty(numOfLines, len(wordDict))
+    # fill the matrix with the amount of times each word appears in each document
+    for i in range(numOfLines):
+        for j in range(len(wordDict)):
+            array[i][j] = documentsTxt[i].count(wordDict[j])
 
     return array
 
